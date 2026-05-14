ff1d0699355fbaeb68115a52fd84bbd3450e6df3
Gustavo Almeida <gustavo.abcdef@hotmail.com>
2026-05-14 18:35:18 +0100

    feat: add ENV var support for integration to kube

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,16 @@ case "$1" in
         ;;
     start)
         echo "[INFO] Running...\n"
-        ./b2bua
+
+        ARGS=()
+        if [[ -n "$LOCAL_SIP_ENDPOINT" ]]; then
+            ARGS+=("--local_addr=$LOCAL_SIP_ENDPOINT")
+        fi
+        if [[ -n "$OUTBOUND_ADDR" ]]; then
+            ARGS+=("--outbound_proxy_addr=$OUTBOUND_ADDR")
+        fi
+
+        ./b2bua "${ARGS[@]}"
         ;;
     *)
         echo "Executing custom command"
